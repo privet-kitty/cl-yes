80c2957814652ed466d001d42941c7840ddef9dc
privet-kitty <privet.kitty99@gmail.com>
2018-12-03 21:24:59 +0100

    Introduce a dispatching macro on string type

--- main.lisp
+++ main.lisp
@@ -4,12 +4,28 @@
   (:export :yes))
 (in-package :cl-yes)
 
-(defun make-buffer (s &optional (times 1000000))
+(defun make-buffer (s &optional (times 10000))
   (with-output-to-string (out)
     (dotimes (n times)
       (write-line s out))))
 
+(defun coerce* (object type)
+  "Returns NIL if COERCE failed."
+  (handler-bind ((type-error (lambda (c)
+                               (declare (ignore c))
+                               (return-from coerce* nil))))
+    (coerce object type)))
+
+(defmacro with-dispatching-on-string (var &body body)
+  (let ((maybe-coerced (gensym)))
+    `(uiop:if-let ((,maybe-coerced (coerce* ,var 'simple-base-string)))
+       (let ((,var ,maybe-coerced))
+         (declare (simple-base-string ,var))
+         ,@body)
+       (progn ,@body))))
+
 (defun yes (out &optional (s "y"))
   (let ((buf (make-buffer s)))
-    (loop
-       (princ buf out))))
+    (with-dispatching-on-string buf
+      (loop
+        (princ buf out)))))
